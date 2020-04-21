187638d5a6b10b88cb440a7e8a1f636a2b5f52f0
Maurício Cordeiro <mauriciocordeiro@live.com>
2020-04-21 11:37:02 -0300

    spring project

---
+++ src/main/java/br/com/mcord/alpr/AlprApplication.java
@@ -0,0 +1,13 @@
+package br.com.mcord.alpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mcord/alpr/AlprApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mcord.alpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

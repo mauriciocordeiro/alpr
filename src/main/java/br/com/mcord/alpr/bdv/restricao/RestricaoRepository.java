b8e3d2d4c28ede7f0b64270796c37d3ff5dd1b1b
Maurício Cordeiro <mauriciocordeiro@live.com>
2020-04-22 11:31:58 -0300

    Update RestricaoRepository.java

--- src/main/java/br/com/mcord/alpr/bdv/restricao/RestricaoRepository.java
+++ src/main/java/br/com/mcord/alpr/bdv/restricao/RestricaoRepository.java
@@ -1,7 +1,7 @@
 package br.com.mcord.alpr.bdv.restricao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface RestricaoRepository extends JpaRepository<Restricao, Integer> {
+public interface RestricaoRepository extends CrudRepository<Restricao, Integer> {
 
 }

af9538e2d1d571d48fda7b4132203e5392a0e02e
Maurício Cordeiro <mauriciocordeiro@live.com>
2020-05-01 21:39:51 -0300

    cors

--- src/main/java/br/com/mcord/alpr/AlprApplication.java
+++ src/main/java/br/com/mcord/alpr/AlprApplication.java
@@ -1,15 +1,20 @@
 package br.com.mcord.alpr;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import br.com.mcord.alpr.filter.JWTAuthorizationFilter;
 
@@ -22,8 +27,9 @@ public class AlprApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlprApplication.class, args);
-	}
+	}	
 	
+	@EnableGlobalMethodSecurity(prePostEnabled = true)
 	@EnableWebSecurity
 	@Configuration
 	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -35,6 +41,13 @@ public class AlprApplication extends SpringBootServletInitializer{
 				.authorizeRequests()
 				.antMatchers(HttpMethod.POST, "/api/login").permitAll()
 				.anyRequest().authenticated();
+			
+			http.cors().configurationSource(new CorsConfigurationSource() {
+	            @Override
+	            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+	                return new CorsConfiguration().applyPermitDefaultValues();
+	            }
+	        });
 		}
 	}
 }
